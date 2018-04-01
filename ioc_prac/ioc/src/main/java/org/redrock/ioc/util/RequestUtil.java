08acbeb8a7b7b98b96b4e60b4164425b091b5ca0
chunglooyo <825411505@qq.com>
2018-04-01 12:21:16 +0800

    request2bean

--- ioc_prac/ioc/src/main/java/org/redrock/ioc/util/RequestUtil.java
+++ ioc_prac/ioc/src/main/java/org/redrock/ioc/util/RequestUtil.java
@@ -2,21 +2,23 @@ package main.java.org.redrock.ioc.util;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Enumeration;
 
 
 public class RequestUtil {
-    public static Object getObject(HttpServletRequest request, Class<?> clazz) {
-        Enumeration<String> names = request.getParameterNames();
-        Object object = null;
+    public static <T> T getObject(HttpServletRequest request, Class<?> clazz) {
+        T object = null;
         try {
-            object = clazz.newInstance();
+            object = (T) clazz.newInstance();
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
+        Enumeration<String> names = request.getParameterNames();
         Field[] fields = clazz.getDeclaredFields();
+        Method[] methods = clazz.getDeclaredMethods();
         while (names.hasMoreElements()) {
             for (Field field : fields) {
                 String fieldName = field.getName();
